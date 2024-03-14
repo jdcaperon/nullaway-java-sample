d91afe9084e50742b1155ffad8d16ef9c63fcb26
Jack Caperon <jack@canva.com>
2024-03-15 07:49:23 +1100

    .

--- lib/src/main/java/net/sridharan/sample/Scratch.java
+++ lib/src/main/java/net/sridharan/sample/Scratch.java
@@ -2,6 +2,7 @@
 
 package net.sridharan.sample;
 
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
 import java.util.ArrayList;
@@ -17,11 +18,22 @@ class Scratch {
       return new Qux(split[0], split[1]);
     }
   }
+
+  // Not fine here, but fine in my environment
   void streams() {
     List<Foo> foos = List.of(new Foo("a-b", "c"));
     foos.stream()
             .filter(f -> f.bar != null)
             // NullAway complains about nullability of bar even though bar is null checked
             .collect(Collectors.toMap(f -> Transformer.transform(f.bar), f -> f.baz));
+  }
+
+  // Fine in this environment but not in mine
+  void streams2() {
+    List<Foo> foos = List.of(new Foo("a-b", "c"));
+    foos.stream()
+            .filter(f -> f.bar != null)
+            // NullAway complains about nullability of bar even though bar is null checked
+            .collect(Collectors.toMap(f -> f.bar, f -> f.baz));
   }
 }
